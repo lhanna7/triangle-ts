7dad42e3e64d406c00039284b27a811ca97381ef
Lauren <lhanna7@ford.com>
2022-05-10 12:13:47 -0700

    solution

--- triangle.ts
+++ triangle.ts
@@ -1,1 +1,55 @@
-export class Triangle {}
+interface ITriangle {
+    isEquilateral: boolean;
+    isIsosceles: boolean;
+}
+
+export class Triangle implements ITriangle {
+    side1: number;
+    side2: number;
+    side3: number;
+
+    constructor(side1: number, side2: number, side3: number) {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    private get isTriangle() {
+        return (
+            this.conformsToSideLaw &&
+            this.side1 > 0 &&
+            this.side2 > 0 &&
+            this.side3 > 0
+        )
+    }
+
+    private get conformsToSideLaw() {
+        return (
+            this.side1 + this.side2 >= this.side3 &&
+            this.side2 + this.side3 >= this.side1 &&
+            this.side3 + this.side1 >= this.side2
+        )
+    }
+
+    get isEquilateral() {
+        return (
+            this.isTriangle && this.side1 === this.side2 && this.side1 === this.side3
+        );
+    }
+
+    get isIsosceles() {
+        return (
+            this.isTriangle &&
+            (this.side2 === this.side3 ||
+                this.side1 === this.side2 ||
+                this.side1 === this.side3)
+        )
+    }
+
+    get isScalene() {
+        return (
+            this.isTriangle &&
+            (this.side1 !== this.side2 && this.side1 !== this.side3)
+        )
+    }
+}
